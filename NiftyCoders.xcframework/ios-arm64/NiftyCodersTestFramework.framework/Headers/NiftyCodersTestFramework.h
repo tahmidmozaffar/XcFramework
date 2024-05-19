866080ba72e7cb4da943e7dc591e1e529a4707db
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2024-05-19 10:18:40 -0400

    init

---
+++ NiftyCoders.xcframework/ios-arm64/NiftyCodersTestFramework.framework/Headers/NiftyCodersTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  NiftyCodersTestFramework.h
+//  NiftyCodersTestFramework
+//
+//  Created by Md Tahmid Mozaffar on 2024-05-18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NiftyCodersTestFramework.
+FOUNDATION_EXPORT double NiftyCodersTestFrameworkVersionNumber;
+
+//! Project version string for NiftyCodersTestFramework.
+FOUNDATION_EXPORT const unsigned char NiftyCodersTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NiftyCodersTestFramework/PublicHeader.h>
+
+
